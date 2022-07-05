9f952830384fe612ae0840417d77b052befade7f
Семен Лаверентьев <remark.lavrentev@gmail.com>
2022-07-05 22:34:05 +0500

    slider

--- markup/components/hero/hero.js
+++ markup/components/hero/hero.js
@@ -2,6 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const slider = new Swiper('.hero', {
         slidesPerView: 'auto',
         spaceBetween: 155,
-        grabCursor: true
+        grabCursor: true,
+        navigation: {
+            nextEl: '.hero .swiper-button-next',
+            prevEl: '.hero .swiper-button-prev'
+        },
+        pagination: {
+            el: ".hero .swiper-pagination",
+            type: "bullets",
+            clickable: true,
+          },
     })
 })
